fc57c087f284ecec30fca8096d5a0763c97c14b3
Luke W Johnston <lwjohnst@gmail.com>
2015-06-24 11:28:28 -0400

    Removed redundance's in plotForest

--- R/plotForest.R
+++ R/plotForest.R
@@ -8,6 +8,7 @@
 ##' will be split up vertically for each grouping.  Thus, a large
 ##' amount of information on the results can be provided in a fairly
 ##' small amount of space.
+##' 
 ##' @title Forest plot
 ##' @param data Dataset for the forest plot.
 ##' @param coefficient,coeff The column that contains the beta
@@ -19,7 +20,8 @@
 ##' @param pvalue.factor.column,dot.pval The column that contains the
 ##' p-value in the form of a factor variable (ie. with levels such as
 ##' '>0.05' and '<0.05').
-##' @param groups The variable to split the plot up.
+##' @param groups The variable to split the plot up, as a formula
+##' (var1 ~ var2, or ~ var2, etc).
 ##' @param y.axis.label,ylab The y-axis label.
 ##' @param x.axis.label,xlab The x-axis label.
 ##' @export
@@ -48,52 +50,41 @@
 ##'   extractBetaFromListGEE() %>%
 ##'   unlistAndFilterIndep(., exposures) %>%
 ##'   createCI() %>%
-##'   plotForest(., dot.pval = 'f.pvalue', groups = 'dep')
+##'   plotForest(., dot.pval = 'f.pvalue', groups = ' ~ dep')
 ##' 
-plotForest <- function(data, coeff = coefficient, yvar = y.variables.column,
-                       ylab = y.axis.label, xlab = x.axis.label,
-                       ci = confid.interval, dot.pval = pvalue.factor.column,
-                       coefficient = 'Estimate',
-                       y.variables.column = 'indep',
-                       confid.interval = c('lowerCI', 'upperCI'),
-                       pvalue.factor.column = 'NULL', groups = NULL,
+plotForest <- function(data, coefficient = 'estimate',
+                       y.axis.variables = 'indep',
+                       confid.interval = c('conf.low', 'conf.high'),
+                       pvalue.factor = NULL, groups = NULL,
                        y.axis.label = 'Exposures',
-                       x.axis.label = 'Beta estimate') {
+                       x.axis.label = 'Beta estimates') {
 
-    ## Make sure each argument has the same values.
-    if (coeff != coefficient | yvar != y.variables.column |
-        ylab != y.axis.label | xlab != x.axis.label | (ci != confid.interval)[1] |
-        dot.pval != pvalue.factor.column) {
-        coefficient <- coeff
-        y.variables.column <- yvar
-        y.axis.label <- ylab
-        x.axis.label <- xlab
-        confid.interval <- ci
-        pvalue.factor.column <- dot.pval
-    }
-
+    ## Uses ggplot2
+    
     ## Main forest plot
     p <- data %>%
-      ggplot(., aes_string(x = coefficient, y = y.variables.column)) +
+      ggplot(., aes_string(x = coefficient, y = y.axis.variables)) +
       geom_errorbarh(aes_string(xmin = confid.interval[1],
                                 xmax = confid.interval[2],
-                                alpha = pvalue.factor.column),
+                                alpha = pvalue.factor),
                      height = 0) +
       geom_vline(xintercept = 0, linetype = 'dashed') +
       labs(y = y.axis.label, x = x.axis.label) +
       theme_bw()
 
     ## Change the size of the dot and line based on significance.
-    if (!is.null(pvalue.factor.column)) {
-        p <- p + geom_point(aes_string(size = pvalue.factor.column,
-                                       alpha = pvalue.factor.column)) +
+    if (!is.null(pvalue.factor)) {
+        p <- p + geom_point(aes_string(size = pvalue.factor,
+                                       alpha = pvalue.factor)) +
           scale_alpha_discrete(name = 'P-value') +
           scale_size_discrete(name = 'P-value')
+    } else {
+        p <- p + geom_point()
     }
 
     ## Split the plot up by a group variable.
     if (!is.null(groups)) {
-        p <- p + facet_grid(reformulate(groups, '.'))
+        p <- p + facet_grid(as.formula(groups))
     }
 
     return(p)
