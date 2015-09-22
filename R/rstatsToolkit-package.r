7c4e3862d273a8f5c5d0752671d1ed0bcea7ce62
Luke W Johnston <lwjohnst@gmail.com>
2015-09-22 15:22:59 -0400

    Removed import parameters from package file

--- R/rstatsToolkit-package.r
+++ R/rstatsToolkit-package.r
@@ -2,12 +2,4 @@
 #'
 #' @name rstatsToolkit
 #' @docType package
-#' @import reshape2
-#' @import ggplot2
-#' @import grid
-#' @import dplyr
-#' @import gee
-#' @import broom
-#' @importFrom data.table rbindlist
-#' @import reshape2
 NULL
